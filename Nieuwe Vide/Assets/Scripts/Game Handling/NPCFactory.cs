1b3dbad84aea00fa2b976a3f68687e7afa749e52
Djamali Shawn Jones <15546@ma-web.nl>
2017-05-23 20:33:06 +0200

    DOTween Reimport & Spawning NPCs on a timer

--- Nieuwe Vide/Assets/Scripts/Game Handling/NPCFactory.cs
+++ Nieuwe Vide/Assets/Scripts/Game Handling/NPCFactory.cs
@@ -8,7 +8,7 @@ public class NPCFactory : MonoBehaviour {
 	public RuntimeAnimatorController[] npcSkins = new RuntimeAnimatorController[5];
 
 	//Just a placeholder for Skins.
-	public Sprite sprite;
+	public Sprite[] sprites = new Sprite[12];
 
 	//The NPC Object to Build.
 	private GameObject _npcGO;
@@ -16,8 +16,8 @@ public class NPCFactory : MonoBehaviour {
 	private SpriteRenderer _spriteRenderer;
 	private Animator _npcAnimator;
 	private BoxCollider2D _boxCollider2D;
+
 	private Victim _victimScript;
-//	private Rigidbody2D _rigidbody2D;
 
 	private int _orderInLayer = -1 | 1;
 
@@ -25,20 +25,18 @@ public class NPCFactory : MonoBehaviour {
 	/// Builds a new npc.
 	/// </summary>
 	/// <returns>The new npc.</returns>
-	public GameObject BuildNewNpc(Vector2 spawnPosition)
+	public GameObject BuildNewNpc()
 	{
 		_npcGO = new GameObject();
 		_npcGO.name = "Victim NPC";
-
-		_npcGO.transform.position = spawnPosition;
-
+		
 		_spriteRenderer = _npcGO.AddComponent<SpriteRenderer>() as SpriteRenderer;
 		_npcAnimator = _npcGO.AddComponent<Animator>() as Animator;
 		_boxCollider2D = _npcGO.AddComponent<BoxCollider2D>() as BoxCollider2D;
-//		_rigidbody2D = _npcGO.AddComponent<Rigidbody2D>() as Rigidbody2D;
+		_victimScript = _npcGO.AddComponent<Victim>();
 
 		_spriteRenderer.sortingOrder = _orderInLayer;
-		_spriteRenderer.sprite = sprite as Sprite;
+		_spriteRenderer.sprite = sprites[ Random.Range(0, sprites.Length) ] as Sprite;
 		_npcAnimator.runtimeAnimatorController = npcSkins[ Random.Range(0, npcSkins.Length) ] as RuntimeAnimatorController;
 		_boxCollider2D.isTrigger = true;
 
